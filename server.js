66dc0d8abac997b3b2d9dc5405e36830a1dfe039
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-10 00:20:01 +0000

    serving index.html when user connects to /

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-const express = remquire("express");
+const express = require("express");
 const path = require("path");
 const fs = require("fs")
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//open index.html 
+app.get ("/", (res, req) =>
+    res.sendFile(path.join(___dirname, "public/index.html"))
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
