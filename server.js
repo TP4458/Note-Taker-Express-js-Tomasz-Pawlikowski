e06d1097ee609a74462a389f67791b28bf345f72
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-10 00:36:21 +0000

    added GET for notes.html

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//open index.html 
-app.get ("/", (res, req) =>
-    res.sendFile(path.join(___dirname, "public/index.html"))
+// open index.html 
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
-
+app.get("/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
 
 
