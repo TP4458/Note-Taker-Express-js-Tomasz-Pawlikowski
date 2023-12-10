2c9a08a9de96fc9cc2007a2e86afaabd3d21328e
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-10 23:22:07 +0000

    added wildcard route

--- server.js
+++ server.js
@@ -8,16 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// open index.html 
-app.get("/", (req, res) =>
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
-);
+});
 
-app.get("/notes", (req, res) =>
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
-);
+});
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html" ))
+})
 
 
 
