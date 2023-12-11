5647776213a48f28d516ddc7055f7729f7011fb8
Tomasz Pawlikowski <tp4458@gmail.com>
2023-12-11 00:02:03 +0000

    get db.json

--- server.js
+++ server.js
@@ -3,11 +3,25 @@ const path = require("path");
 const fs = require("fs")
 const PORT = process.env.PORT || 3001;
 const app = express();
+const uniqid = require("uniqid");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
+
+
+//api routes
+app.get("/api/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "db/db.json"))
+});
+
+
+
+
+//html routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 });
@@ -15,20 +29,9 @@ app.get("/", (req, res) => {
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 });
-
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html" ))
 })
-
-
-
-
-
-
-
-
-
-
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
